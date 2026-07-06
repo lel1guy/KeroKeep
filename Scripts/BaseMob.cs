c2fa7c734c5ab77ac4b7ed1b8f8a30f6f81e5430
lel1guy <vvareiro@gmail.com>
2026-07-06 22:25:39 +0100

    feat: add BaseMob class for enemy behavior and update Skeleton scene reference

---
+++ Scripts/BaseMob.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+namespace KeroKeep
+{
+
+public partial class BaseMob : CharacterBody2D
+    {
+
+        private GameManager _gameManager;
+
+        [Export] public int Health { get; set; } = 1;
+        [Export] public int Speed { get; set; } = 50;
+        [Export] public int GoldDrop { get; set; } = 1;
+
+        public int Damage { get; set; }
+
+        private AnimatedSprite2D _sprite;
+        public override void _Ready()
+        {
+            _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+            _sprite.Play("Walk");
+
+            _gameManager = GetNode<GameManager>("/root/GameManager");
+        }
+
+        public override void _PhysicsProcess(double delta)
+        {
+            Velocity = new Vector2(0, Speed);
+            MoveAndSlide();
+        }
+
+        public async void TakeDamage(float damage)
+        {
+            Health -= (int)damage;
+            if (Health <= 0)
+            {
+                Speed = 0;
+                _gameManager.AddGold(GoldDrop * (int)_gameManager.GoldDropMultiplier);
+                _sprite.Play("Death");
+                await ToSignal(_sprite, AnimatedSprite2D.SignalName.AnimationFinished);
+                QueueFree();
+            }
+        }
+
+        public override void _InputEvent(Node viewport, InputEvent @event, long shapeIdx)
+        {
+            if (Health > 0 && @event.IsActionPressed("clickAttack"))
+            {
+                TakeDamage(_gameManager.ClickDamage);
+            }   
+        }
+    }
+}
